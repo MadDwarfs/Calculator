6c2fdea8e5d73456677854081ccf5a00fcfe36b1
MadDwarfs <jakubmon0@gmail.com>
2020-05-30 17:18:49 +0200

    Second calculator commit

--- src/test/java/com/calculator/kodillacalculator/Calculator.java
+++ src/test/java/com/calculator/kodillacalculator/Calculator.java
@@ -1,4 +1,24 @@
 package com.calculator.kodillacalculator;
 
+
+
 public class Calculator {
+
+    public double addition(double a,double b){
+        System.out.println("The sum of this addition is: ");
+        return a+b;
+    }
+
+    public double substraction(double a,double b){
+        System.out.println("The result of this substraction is: ");
+        return a-b;
+    }
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.addition(25.5,76));
+        System.out.println(calculator.substraction(52,57));
+    }
+
 }
